ad1c46d8dfa1abc9b7b1bfaf529c3682487d6b9d
Domonkos Lezsak <lezsakdomi1@gmail.com>
2022-02-08 09:07:10 +0100

    sh: improve process_feh.sh

--- process_feh.sh
+++ process_feh.sh
@@ -1,4 +1,5 @@
 folder="$(wmctrl -l|awk '{$3=""; $2=""; $1=""; print $0}' | grep -oP 'feh \[\d+ of \d+\] - \K.*_Files' || exit 1)"
 fn="$(find "$folder/" -name '*_c0.tif' | head -n1)"
-echo "Found $fn"
+echo "\e[1mFound $fn\e[0m"
 python pipeline.py "${fn%_c0.tif}_c{}.tif"
+echo "$folder"
