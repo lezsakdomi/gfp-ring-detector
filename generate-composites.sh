7e52cce97b7b8bcc500c0e36ea6ebbf0e19c1ee4
Domonkos Lezsak <lezsakdomi1@gmail.com>
2022-04-19 19:52:45 +0200

    sh: generate-composites.sh: Make script use a wildcard rather than individual images
    Works only if channels are in RGB
    
    Further work needed to use dataset.toml files

--- generate-composites.sh
+++ generate-composites.sh
@@ -5,7 +5,7 @@ echo "$files" | while IFS= read file; do
   let i++
   printf "$i / $count\r"
   folder="$(dirname "$file")"
-  convert "$folder"/*_c1.tif "$folder"/*_c2.tif "$folder"/*_c0.tif -combine "$folder"/composite.tif
+  convert "$folder"/*_c?.tif -combine "$folder"/composite.tif
   convert "$folder"/composite.tif "$folder"/composite.jpg
 done
 echo
